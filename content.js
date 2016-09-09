b9c281d9bf0e75c552bc2878d42e71749391b373
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-09-09 14:14:04 -0600

    Revert "Revert "base setup""
    This reverts commit 7caca40ee23a270a03a2f426d4ea762acdc2be0b.

---
+++ content.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ben.Vaughan on 9/9/2016.
+ */
+    module.exports = "It works from content.js!";
---
+++ entry.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Ben.Vaughan on 9/9/2016.
+ */
+    require("./style.scss");
+    document.write(require("./content.js"));

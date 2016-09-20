b80e9fa8b19d1c5cf440f1bdfbad226c122c6268
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-09-20 07:26:38 -0600

    moving to gulp
    switching to a gulp based build chain.

--- src/js/entry.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Ben.Vaughan on 9/9/2016.
- */
-require('file?name=[name].[ext]!../index.html');
-require("../sass/style.scss");
-require('bootstrap-loader');
-
-document.write(require("./content.js"));
